b1ece9625b8dd603e9d22399c2ffc12a6ccb88eb
steve-the3cloud <steve@the3.cloud>
2025-02-28 01:48:41 +0800

    add build verifier

--- crates/output-solidity/src/groth16/mod.rs
+++ crates/output-solidity/src/groth16/mod.rs
@@ -1,8 +1,25 @@
+use anyhow::Result;
+use proof_forge_core::groth16::VerificationKey;
+
 mod build_vkey;
-pub use build_vkey::*;
 
 mod build_linear;
-pub use build_linear::*;
 
 mod build_validate;
-pub use build_validate::*;
+
+pub fn build_verifier(vkey: &VerificationKey) -> Result<String> {
+    let s = include_str!("verifier.sol.template");
+
+    let s = s
+        .replace("<%verification_key_data%>", &build_vkey::build_vkey(vkey)?)
+        .replace(
+            "<%linear_combination_vk_x%>",
+            &build_linear::build_linear(vkey)?,
+        )
+        .replace(
+            "<%validate_evaluations%>",
+            &build_validate::build_validate(vkey)?,
+        );
+
+    Ok(s)
+}
