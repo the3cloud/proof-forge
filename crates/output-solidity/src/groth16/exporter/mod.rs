54133847656e441524a732ba1dbd3e6cb6a6f929
steve-the3cloud <steve@the3.cloud>
2025-02-28 19:47:17 +0800

    clean code

---
+++ crates/output-solidity/src/groth16/exporter/mod.rs
@@ -0,0 +1,39 @@
+use anyhow::Result;
+use proof_forge_core::groth16::VerificationKey;
+
+mod build_linear;
+mod build_validate;
+mod build_vkey;
+
+pub fn build_verifier(vkey: &VerificationKey) -> Result<String> {
+    let s = include_str!("../verifier.sol.template");
+
+    let s = s
+        .replace("<%verification_key_data%>", &build_vkey::build_vkey(vkey)?)
+        .replace(
+            "<%linear_combination_vk_x%>",
+            &build_linear::build_linear(vkey)?,
+        )
+        .replace(
+            "<%validate_evaluations%>",
+            &build_validate::build_validate(vkey)?,
+        )
+        .replace("<%n_public%>", &format!("{}", vkey.n_public()));
+
+    Ok(s)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_build_verifier() {
+        let vkey: VerificationKey =
+            serde_json::from_str(include_str!("../../../testdata/verifying_key.json")).unwrap();
+
+        let s = build_verifier(&vkey).unwrap();
+
+        println!("{}", s);
+    }
+}
---
+++ crates/output-solidity/src/groth16/proof.rs
@@ -0,0 +1,8 @@
+use anyhow::Result;
+use proof_forge_core::groth16::Proof;
+
+pub fn build_proof(proof: &Proof) -> Result<Vec<u8>> {
+    let mut v = Vec::new();
+
+    Ok(v)
+}
